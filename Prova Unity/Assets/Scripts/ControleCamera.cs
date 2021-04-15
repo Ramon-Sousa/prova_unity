cca17bcad69eee89ba52e5b05ab639cc34a55dbc
Ramon Sousa <ramon.sousa_@hotmail.com>
2021-04-15 14:46:09 -0300

    Entrega avaliação

---
+++ Prova Unity/Assets/Scripts/ControleCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleCamera : MonoBehaviour
+{
+
+	public GameObject jogador;
+	private Vector3 offset;	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	Vector3 posicao = new Vector3(jogador.transform.position.x, 0.0f, jogador.transform.position.z);
+        transform.position = jogador.transform.position = posicao + offset;
+    }
+}
---
+++ Prova Unity/Assets/Scripts/ControleJogador.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+
+//adiciona o componente CharacterController automaticamente
+[RequireComponent(typeof(CharacterController))]
+
+public class ControleJogador : MonoBehaviour
+{
+    public float speed = 7.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // Se o jogador estiver no chão, então pode se mover
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX =  speed * Input.GetAxis("Vertical");
+            float curSpeedY = speed * Input.GetAxis("Horizontal");
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Aplica gravidade
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move o jogador
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Gira a Câmera
+        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        transform.eulerAngles = new Vector2(0, rotation.y);
+    }
+}
+
